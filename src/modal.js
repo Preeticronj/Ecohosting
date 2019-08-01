66ae6a8d61f0f153451c01144d91a11f21ac047e
Shruticronj <preeti@cronj.com>
2019-08-01 12:48:50 +0530

    modal file added

---
+++ src/modal.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Modal from 'react-awesome-modal';
+import './style.css';
+import { ModalForm } from './index';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <Modal 
+                visible={this.props.visible}
+                width="469"
+                height="552"
+                effect="fadeInUp" 
+                backgroundColor="transparent"
+                onClickAway={() => this.props.closeModal()}
+                >
+                <div className="signup_modal">
+                    <ModalForm
+                        signupErrors={this.props.signupErrors}
+                        signup_active={this.props.signup_active} 
+                        oncloseModal={this.props.oncloseModal}
+                        allDropdowns={this.props.allDropdowns}
+                        addUser={this.props.addUser}
+                        is_edit={this.props.is_edit}
+                        add_user = {this.props.add_user}
+                        user={this.props.user}
+                        updateUser={this.props.updateUser}
+                        isUserExist={this.props.isUserExist}
+                        />
+                    <a href="javascript:void(0);" onClick={() => this.props.closeModal()}></a>
+                   
+                </div>
+            </Modal>
+        );
+    }
+}
+
+
+export default App
